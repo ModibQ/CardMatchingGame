c3c729cbbe404a40b50d8c5c4ce0c42de2d0f02e
Alex Kashian <Alex_Kashian@student.uml.edu>
2022-12-05 21:36:07 -0500

    Updated buttons and bug fixing the game logic for the beginner level

--- app/src/main/java/com/mobileapp/finalproject/BeginnerFragment.java
+++ app/src/main/java/com/mobileapp/finalproject/BeginnerFragment.java
@@ -8,17 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 import com.mobileapp.finalproject.databinding.FragmentBeginnerBinding;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Random;
 
 
 public class BeginnerFragment extends Fragment {
-
+    int i;
+    int clicked = 0;
+    boolean turnOver = false;
+    int lastClicked = -1;
     private FragmentBeginnerBinding binding;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,22 +29,66 @@ public class BeginnerFragment extends Fragment {
                 container, false);
         View view = binding.getRoot();
 
-        List<Integer> img = Arrays.asList(R.drawable.chef, R.drawable.chef,
+
+        int[] img = new int[]{R.drawable.chef, R.drawable.chef,
                 R.drawable.cop,R.drawable.cop,
                 R.drawable.hero,R.drawable.hero,
-                R.drawable.king,R.drawable.king);
+                R.drawable.king,R.drawable.king};
 
-        ImageButton[] buttons= new ImageButton[]{binding.beginner1,binding.beginner2,binding.beginner3,binding.beginner4,
-                binding.beginner5,binding.beginner6,binding.beginner7,binding.beginner8};
+        Button[] buttons= new Button[]{binding.beginner1,binding.beginner2,binding.beginner3,binding.beginner4,
+              binding.beginner5,binding.beginner6,binding.beginner7,binding.beginner8};
 
 
-        Collections.shuffle(img);
 
-        for(int i = 0; i < 8; i++){
-            buttons[i].setBackgroundResource(R.drawable.chef);
+
+
+        Random rand = new Random();
+
+        for (int i = 0; i < img.length; i++) {
+            int randomIndexToSwap = rand.nextInt(img.length);
+            int temp = img[randomIndexToSwap];
+            img[randomIndexToSwap] = img[i];
+            img[i] = temp;
+        }
+        System.out.println(Arrays.toString(img));
+
+        for(i = 0; i < buttons.length-1; i++){
+            buttons[i].setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,null,getResources().getDrawable(R.drawable.code));
+            System.out.println(buttons[i].getText());
+            //buttons[i].setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,null,getResources().getDrawable(img[i]));
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(buttons[i].getText() == "cardback" && !turnOver){
+                        buttons[i].setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,null,getResources().getDrawable(img[i]));
+                        buttons[i].setText(img[i]);
+                        System.out.println("CHECK");
+                        if (clicked == 0) {
+                            lastClicked = i;
+                        }
+                        clicked++;
+                    } else if (buttons[i].getText() != "cardback") {
+                        buttons[i].setBackgroundResource(R.drawable.code);
+                        buttons[i].setText("cardback");
+                        clicked--;
+                    }
+                    if (clicked == 2) {
+                        turnOver = true;
+                        if (buttons[i].getText() == buttons[lastClicked].getText()) {
+                            buttons[i].setEnabled(false);
+                            buttons[lastClicked].setEnabled(false);
+                            turnOver = false;
+                            clicked = 0;
+                        }
+                    } else if (clicked == 0) {
+                        turnOver = false;
+                    }
+                }
+            });
         }
 
-        return inflater.inflate(R.layout.fragment_beginner, container, false);
+        return view;
 
 
     }
--- app/src/main/java/com/mobileapp/finalproject/WelcomeFragment.java
+++ app/src/main/java/com/mobileapp/finalproject/WelcomeFragment.java
@@ -3,23 +3,40 @@ package com.mobileapp.finalproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.mobileapp.finalproject.databinding.FragmentExpertBinding;
 import com.mobileapp.finalproject.databinding.FragmentWelcomeBinding;
 
 
 public class WelcomeFragment extends Fragment {
     private FragmentWelcomeBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentWelcomeBinding.inflate(inflater,container, false);
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_beginnerFragment);
+            }
+        });
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_intermediateFragment);
+            }
+        });
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_expertFragment);
+            }
+        });
+        return view;
     }
 }
